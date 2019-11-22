f30eac9440be4c627673aeb0ce51387626a47ddb
Romulo <rdbarbos@firjan.com.br>
2019-11-22 10:51:35 -0300

    Versionamento do projeto

---
+++ src/Interface/IJobService.cs
@@ -0,0 +1,17 @@
+﻿using CoreDemoDapper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemoDapper.Interface
+{
+    public interface IJobService
+    {
+        int Delete(int JobId);
+        Job GetByJobId(int JobId);
+        string Update(Job job);
+        int Create(Job JobDetails);
+        List<Job> ListAll();
+    }
+}
